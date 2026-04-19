c29eb4615c607e33da3e5bbb4ee1aa5fb945049a
Max-Beier <max@maxbeier.dev>
2026-04-19 17:54:14 +0200

    Removed cloudlare KV

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
-  adapter: cloudflare({
-    platformProxy: true,
-  }),
-  output: "server",
 });
